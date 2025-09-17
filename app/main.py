466ad055eda4b24aec9902e35a81efde59648c66
andre-maia51 <acmc.0410@gmail.com>
2025-09-17 11:13:33 -0300

    feat: add linter configuration

--- app/main.py
+++ app/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 
 app = FastAPI(title="Estrato API")
 
+
 @app.get("/")
 def read_root():
-    return {"status": "ok", "message":"API is working!"}
+    return {"status": "ok", "message": "API is working!"}
